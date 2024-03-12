869c84b47f73560b8def3b9603305d7215af1a16
unknown <marlonhenrique.santos.ms@gmail.com>
2024-03-12 16:49:35 -0300

    POO simples

---
+++ Person/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    internal class Person
+    {
+        public class Pessoa
+        {
+            public string Name { get; set; }
+
+            public int Idade { get; set; }
+
+            public string Profissao { get; set; }
+
+            public int Salario { get; set; }
+
+            
+            public void Apresentar()
+            {
+                Console.Write($"Olá me chamo {Name} tenho {Idade} anos e sou {Profissao} e ganho {Salario}");
+            }
+        }
+
+        
+
+
+    }
+}
